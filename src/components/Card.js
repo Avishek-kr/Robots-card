196c2c7584969730e552e66cd9d0cbf84c3394bf
Avishek-kr <ak251997@gmail.com>
2021-03-20 20:53:12 +0530

    created 1st card structure

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <img alt="robots" src="https:/robohash.org/test"/>
+            <div>
+                <h2>Av$hek</h2>
+                <p>ak251997@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons'
 import reportWebVitals from './reportWebVitals';
+import Card from './components/Card';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App  />
+    <Card  />
   </React.StrictMode>,
   document.getElementById('root')
 );
